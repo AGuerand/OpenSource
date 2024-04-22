7feb65493a48cdd0e18d5d14ae57b498992ad020
AGuerand <anthony.guerand@ynov.com>
2024-04-22 06:45:00 -0400

    1 2 et 3 finis

--- right.py
+++ right.py
@@ -4,7 +4,7 @@ def perm(path, user, group, other):
 
     try:
         os.chmod(path, (user * 8 * 8) + (group * 8) + other)
-        print("Autorisations définies avec succès pour le fichier", path)
+        print("Succès\n", path)
     except Exception as e:
-        print("Une erreur s'est produite lors de la définition des autorisations:", str(e))
+        print("Erreur:\n", str(e))
 
