ca4394b8e8f8b16e255f75521644ae9cf574674d
Halima EL GANTOUH <helgantouh@evesio.fr>
2024-04-25 21:56:42 +0200

    halima

--- Front/DoubleAuthentification/Redirect.py
+++ Front/DoubleAuthentification/Redirect.py
@@ -2,12 +2,18 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def login_redirect():
-    return redirect(url_for('authentification'))
+def authentification():
+    return render_template('Authentification.html')
 
 @app.route('/doubleAuthentification')
 def double_authentification():
-    return render_template('doubleAuthentification/doubleAuthentification.html')
+    return render_template('doubleAuthentification.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    # Ici, vous pouvez ajouter le code pour vérifier l'authentification de l'utilisateur
+    # Si l'authentification réussit, vous pouvez rediriger vers la page de double authentification
+    return redirect(url_for('double_authentification'))
 
 if __name__ == '__main__':
     app.run(debug=True)
